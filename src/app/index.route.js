fd3cc07dbebbd16621f1cd415cdf78adbeebd8a9
Teng <843018739@qq.com>
2016-04-03 15:56:00 +0800

    update README & complete navbar

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,17 +8,48 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('app', {
         url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+        views: {
+            'header': {
+                templateUrl: 'app/components/header/header.html'
+            },
+            'navbar': {
+                templateUrl: 'app/components/navbar/navbar.html'
+            },
+            'sub-navbar': {
+                templateUrl: 'app/components/sub-navbar/user-list-sub-navbar.html'
+            },
+            'content': {
+                templateUrl: 'app/user-list/user-list.html',
+                controller: 'MainController',
+                controllerAs: 'userList'
+            },
+            'footer': {
+                templateUrl: 'app/components/footer/footer.html'
+            }
+        }
       })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/login/login.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+      .state('app.login', {
+        url: 'login',
+        views: {
+            'header@': {
+                templateUrl: 'app/blank/blank.html'
+            },
+            'navbar@': {
+                templateUrl: 'app/blank/blank.html'
+            },
+            'sub-navbar@': {
+                templateUrl: 'app/blank/blank.html'
+            },
+            'content@': {
+                templateUrl: 'app/login/login.html',
+                controller: 'LoginCtrl'
+            },
+            'footer@': {
+                templateUrl: 'app/blank/blank.html'
+            }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
