27e663d68e2725a63af7ecb9e712ee961e783a7f
啊顺 <lshunran@163.com>
2016-05-09 14:09:41 +0800

    connect advert ads page

--- src/app/advert-audit/advert-audit.controller.js
+++ src/app/advert-audit/advert-audit.controller.js
@@ -9,7 +9,16 @@
     .controller('AdvertAuditCtrl', AdvertAuditCtrl);
 
   /** @ngInject */
-  function AdvertAuditCtrl() {
+  function AdvertAuditCtrl($http, $scope) {
+  	
+  	$http({
+      method:'post',
+      url:'http://121.42.57.59:4000/back/advert/list/all',
+      data:{token:"72f171533bce44b1640330690f086e21d5fbb58638b8681c3f31314bf4453b8e",tag:1}
+    }).success(function(res){
+      console.log(res);
+      $scope.advertList = res.AdsList;
+    })
 
   }
 })();
