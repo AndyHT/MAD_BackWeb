09335ef3aed4aafe9accacc85b778a14d49ea172
啊顺 <lshunran@163.com>
2016-05-15 21:02:09 +0800

    fix bug of show ads details

--- src/app/advert-audit/advert-audit.controller.js
+++ src/app/advert-audit/advert-audit.controller.js
@@ -37,49 +37,51 @@
    		id: id
    	}).$promise.then(
    	function (response){
-   		function locations(obj) {
-			for (var key in obj) {}
-		}
    		console.log(response.adsDetail);
    		$rootScope.ad_title = response.adsDetail.title;
    		$rootScope.ad_content = response.adsDetail.content;
    		$rootScope.ad_catalog = response.adsDetail.catalog;
    		$rootScope.ad_startDate = response.adsDetail.startDate;
    		$rootScope.ad_endDate = response.adsDetail.endDate;
-   		$rootScope.broadcastLocation = response.adsDetail.broadcastLocation;
    		var location = response.adsDetail.broadcastLocation;
-   		var item = new locations();
-   		for(var i = 0; i<location.length; i++){
-
-   		// item.wujiaochang = false;
-   		// item.renminguangchang = false; 
-   		// item.xujiahui= false;
-   		// item.jingansi= false;
-   		// item.lujiazui= false;
-   		// item.shijigongyuan= false;
-   		// item.zhongshangongyuan= false;
-   		// item.zhangjiang= false;
-   		// item.nanjingdonglu= false;
-   		// item.hongqiao= false;
-   		// item.pudongjichang= false;
-   		// item.wujiaochang = (location[i] == "五角场")&&(item.wujiaochang == false)?true:false;
-   		// item.renminguangchang = (location[i] == "人民广场")&&(item.renminguangchang == false)?true:false;
-   		// item.xujiahui = (location[i] == "徐家汇")&&(item.xujiahui == false)?true:false;
-   		// item.jingansi = (location[i] == "静安寺")&&(item.jingansi == false)?true:false;
-   		// item.lujiazui = (location[i] == "陆家嘴")&&(item.lujiazui == false)?true:false;
-   		// item.shijigongyuan = (location[i] == "世纪公园")&&(item.shijigongyuan == false)?true:false;
-   		// item.zhongshangongyuan = (location[i] == "中山公园")&&(item.zhongshangongyuan == false)?true:false;
-   		// item.zhangjiang = (location[i] == "张江")&&(item.zhangjiang == false)?true:false;
-   		// item.nanjingdonglu = (location[i] == "南京东路")&&(item.nanjingdonglu == false)?true:false;
-   		// item.hongqiao = (location[i] == "虹桥")&&(item.hongqiao == false)?true:false;
-   		// item.pudongjichang = (location[i] == "浦东机场")&&(item.pudongjichang == false)?true:false;
-   		console.log(item);
-
-
-
-   	}
-   	item.wujiaochang = "wujiaochang";
-   	$rootScope.showLocation = item;
+      console.log(location.length);
+      var showLocation = "";
+      for (var i = 0; i < location.length; i++) {
+        if(location[i] == "001"){
+          showLocation = showLocation + "嘉定区 ";
+        }else if(location[i] == "002"){
+          showLocation = showLocation + "金山区 ";
+        }else if(location[i] == "003"){
+          showLocation = showLocation + "奉贤区 ";
+        }else if(location[i] == "004"){
+          showLocation = showLocation + "松江区 ";
+        }else if(location[i] == "005"){
+          showLocation = showLocation + "青浦区 ";
+        }else if(location[i] == "006"){
+          showLocation = showLocation + "闵行区 ";
+        }else if(location[i] == "007"){
+          showLocation = showLocation + "浦东新区 ";
+        }else if(location[i] == "008"){
+          showLocation = showLocation + "长宁区 ";
+        }else if(location[i] == "009"){
+          showLocation = showLocation + "黄浦区 ";
+        }else if(location[i] == "010"){
+          showLocation = showLocation + "宝山区 ";
+        }else if(location[i] == "011"){
+          showLocation = showLocation + "虹口区 ";
+        }else if(location[i] == "012"){
+          showLocation = showLocation + "杨浦区 ";
+        }else if(location[i] == "013"){
+          showLocation = showLocation + "崇明县 ";
+        }else if(location[i] == "014"){
+          showLocation = showLocation + "徐汇区 ";
+        }else if(location[i] == "015"){
+          showLocation = showLocation + "静安区 ";
+        }else if(location[i] == "016"){
+          showLocation = showLocation + "普陀区 ";
+        }
+      };
+   		$rootScope.broadcastLocation = showLocation;
 
 
    	}, function (error) {
